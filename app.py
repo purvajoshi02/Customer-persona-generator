994f85643b64d3e6722b41e001fa91559066a106
Purva Joshi <jpurva63@gmail.com>
2026-07-06 00:10:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,8 @@ def home():
         persona=persona
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
